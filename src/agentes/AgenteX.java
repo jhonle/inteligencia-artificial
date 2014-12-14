6d7df3f6b62ddd9177fcf3576cfa2758440fb973
marcelo <marcelo@marce-pc>
2014-12-14 08:04:05 -0400

    add agenteX.comportamientoImprimir

--- src/agentes/AgenteX.java
+++ src/agentes/AgenteX.java
@@ -1,11 +1,16 @@
 package agentes;
 
 import jade.core.Agent;
+import jade.core.behaviours.SimpleBehaviour;
+import jade.lang.acl.ACLMessage;
 
 import java.util.ArrayList;
-import java.util.Vector;
 
+import agentes.Receptor.ReceptorComportaminento;
 import agentes.emisor.EmisorComportaminento;
+
+
+
 
 public class AgenteX extends Agent {
   
@@ -14,11 +19,22 @@ public class AgenteX extends Agent {
    * por ahora solo la de 1 dia(00:00 hrs a 23:30 hrs)
    * */
   private ArrayList<Actividad> agenda;
-  private static int INTERVALO_DE_TIEMPO=30; //INTERVALO DEL TIEMPO PARA LA AGENDA, EN MINUTOS
+  private static int INTERVALO_DE_TIEMPO=60; //INTERVALO DEL TIEMPO PARA LA AGENDA, EN MINUTOS
  
   protected void setup()
   {
-	  llenarAgenda();
+     llenarAgenda();
+     
+     // espero antes de imprimir para que no se solape con los mensajes de jade
+     try {
+		Thread.sleep(4000);
+	 } catch (InterruptedException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	 }
+     
+     addBehaviour(new comportaminentoImprimir(agenda));
+
      
   }
  
@@ -41,62 +57,57 @@ public class AgenteX extends Agent {
 	      }
 	   }
   }
- 
- /*
-  *este metodo muestra por consola la agenda de un dia 
-  *talvez pueda serbir para mostrar por interfaz grafica
-  * */
- public void imprimirAgenda()
- {		  
-	 System.out.println("HRS         | DETALLE      | DISPONIBLE");
-	 System.out.println("------------|--------------|-----------");
+}//fin clase AgenteX
 
-	 for (int i=0;i<agenda.size();i++)
-	  {
-		 
-	      int hora = (int) agenda.get(i).getHora();
-	      int minutos;
-	      if(hora<10) 
-	      {
-	    	  minutos = (int)((agenda.get(i).getHora()*10)%10);
-	    	  System.out.println("hrs 0"+hora+":"+minutos+"0   |");
-	    	  
-	      }else
-	       {
-	    	    minutos =(int)((agenda.get(i).getHora()*100)%100);
-	    	    if(minutos==0)System.out.println("hrs "+hora+":"+minutos+"0   |" );
-	    	    else System.out.println("hrs "+hora+":"+minutos+"   |");
-	       }
-	      
-	   } 
-	 
- }
- 
- 
- 
-
-
-/*
- * Este metodo solo para probar 
- */
- public AgenteX()
- {
-	llenarAgenda();
+/**
+ * Comportamiento para  imprimir la agenda de una agente
+ * 
+ * */
+class comportaminentoImprimir extends SimpleBehaviour
+{     ArrayList<Actividad>agenda;
+      public comportaminentoImprimir(ArrayList<Actividad>agenda) {
+		this.agenda=agenda;
+	  }
 	
- } 
+	   private boolean fin = false;
+        public void action()
+        {
+          imprimirAgenda();
+          fin = true;
+        }
+        public boolean done()
+        {
+            return fin;
+        }
+        
+        
+        /*
+         *este metodo muestra por consola la agenda de un dia 
+         *talvez pueda serbir para mostrar por interfaz grafica
+         * */
+        public void imprimirAgenda()
+        {		  
+       	 System.out.println("HRS         | DETALLE      | DISPONIBLE");
+       	 System.out.println("------------|--------------|-----------");
 
-/*
- * metodo solo para probar.
- */
- public static void main(String[]args )
- {
-	   AgenteX agent= new AgenteX();
-	  /*
-	  for (int i=0;i<agent.agenda.size();i++)
-	  {
-		  System.out.println("hrs "+agent.agenda.get(i).getHora());
-	  } */
-	   agent.imprimirAgenda();
-  }
-   
+       	 for (int i=0;i<agenda.size();i++)
+       	  {
+       		  int hora = (int) agenda.get(i).getHora();
+       	      int minutos;
+       	      if(hora<10) 
+       	      {
+       	    	  minutos = (int)((agenda.get(i).getHora()*10)%10);
+       	    	  System.out.println("hrs 0"+hora+":"+minutos+"0   |");
+       	    	  
+       	      }else
+       	       {
+       	    	    minutos =(int)((agenda.get(i).getHora()*100)%100);
+       	    	    if(minutos==0)System.out.println("hrs "+hora+":"+minutos+"0   |" );
+       	    	    else System.out.println("hrs "+hora+":"+minutos+"   |");
+       	       }
+       	    } 
+       	 
+        }
 }
+
+
