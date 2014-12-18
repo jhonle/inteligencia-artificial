1ff8049c9552b1d6340d2e45514110f67d18fab1
jhon <jhon_li_@hotmail.com>
2014-12-18 07:57:48 -0300

    agente

---
+++ src/agentes/AgentePrincipal.java
@@ -0,0 +1,36 @@
+package agentes;
+
+import jade.core.*;
+import jade.core.Runtime;
+import jade.wrapper.AgentController;
+import jade.wrapper.ContainerController;
+
+public class AgentePrincipal extends Agent 
+{
+   protected void setup()
+   {
+	   
+	   VentanaPrincipal v = new VentanaPrincipal();
+	   
+	   Runtime rt = Runtime.instance();
+
+       Profile p = new ProfileImpl();
+ 
+       ContainerController mainContainer = rt.createMainContainer(p);
+
+		try 
+		{
+			AgenteX a;
+	       AgentController ac = mainContainer.createNewAgent("Agente1", a,);
+  	       ac.start();
+	    }
+		catch (jade.wrapper.StaleProxyException e) 
+		{
+	        System.err.println("Error launching agent...");
+	    }
+   }
+   protected void takeDown() 
+	{
+	   doDelete();
+	}
+}
